f84446b1fdd1e152f6dfa9b0a3892477cd32de52
Phillip Maree <Phillip.Maree@sintef.no>
2020-05-07 22:48:59 +0200

    Working embedded MPC ctrl
    Needs zero sample and hold

--- 01_cstr_introduction/Simulator/Resources/pull_resources.sh
+++ 01_cstr_introduction/Simulator/Resources/pull_resources.sh
@@ -2,5 +2,6 @@
 rm ./Library/*.*
 rm ./Include/*.*
 
-cp /home/johannes/work/dev/CC-dev/eclipse-workspace/01_mpc_cstr/build/libnmpc.* ./Library/ 
-#cp /home/johannes/work/dev/CC-dev/eclipse-workspace/01_mpc_cstr/include/fmu_mpc.h ./Include/
+cp /home/johannes/work/dev/CC-dev/eclipse-workspace/01_mosiop/build/lib/*.* ./Library/ 
+cp /home/johannes/work/dev/CC-dev/eclipse-workspace/01_mosiop/solver/inc/nlp.h ./Include/
+
